20fcf9baba58b9894596d140b068608291169770
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-11-26 11:16:44 -0800

    Moved firease to js file

---
+++ js/db.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyAXs5EqmXzHzw49sUOrgFE-4B2q83qePPg",
+authDomain: "boxes-d9710.firebaseapp.com",
+projectId: "boxes-d9710",
+storageBucket: "boxes-d9710.appspot.com",
+messagingSenderId: "21099836371",
+appId: "1:21099836371:web:b81cba976511f42386b526"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
